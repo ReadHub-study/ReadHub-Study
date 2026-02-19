dc865088b911fee7d25d48e4f16279b5afca5975
Emiju-Samuel <samuelemiju@gmail.com>
2026-02-18 20:01:40 -0800

    Fixing the google signin error

--- ReadHub_Frontend/src/Pages/Explore.jsx
+++ ReadHub_Frontend/src/Pages/Explore.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Explore = () => {
   return (
-    <div>Explore</div>
+    <>
+    <div className='p-5 min-h-screen bg-gray-100'>
+      <div>
+        <span>Explore</span>
+        <span>Discover new reads and tips</span>
+      </div>
+    </div>
+    </>
   )
 }
 
